9f58a4f83451c24d52767ee6208be2b7388a039b
Martin William <martinsoftwaredev.outlook.com>
2022-09-28 10:06:35 +0100

    add error

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import { Route, Routes } from 'react-router-dom';
 import './App.css';
+import Err from './pages/Err';
 import Home from './pages/Home';
 import Review from './pages/Review';
 
@@ -9,6 +10,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />}></Route>
         <Route path="/reviews/:review_id" element={<Review />}></Route>
+        <Route path="*" element={<Err />} />
       </Routes>
     </div>
   );
